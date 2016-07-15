c4b0edb096ea5478d44a6544393ccb4cd29da3ff
Ulf Swedin <ulf@swedin.nu>
2016-07-15 11:31:47 +0200

    Add DELETE todos

--- server.js
+++ server.js
@@ -52,8 +52,25 @@ app.post('/todos', function (req, res) {
 });
 
 
+// http-method DELETE
+app.delete('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo){
+		res.status(404).json({"error":"no todo found with that id"});
+	}
+	else
+	{
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+	
+});
+
+
 app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
+	console.log('Express Webserver is listening on port ' + PORT + '!');
 });
 
 

08aadb15ebfb24938e026b784de2b4fd265c4184
Ulf Swedin <ulf@swedin.nu>
2016-07-15 11:01:01 +0200

    Refactor API with Underscore

--- server.js
+++ server.js
@@ -1,60 +1,59 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-
 var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-	res.send('TODO API Root');
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
 });
 
 // GET /todos
-app.get('/todos', function(req, res){
-	//JSON
+app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-app.get('/todos/:id', function(req,res){
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo; //Undefined
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function(todo){
-		if (todoId===todo.id){
-			matchedTodo = todo;
-		}
-	});
-
-	// Undefined is falsey
 	if (matchedTodo) {
 		res.json(matchedTodo);
-	}
-	else {
+	} else {
 		res.status(404).send();
 	}
-	//Iterate array, find the match
-	//res.send('Asking for id of ' + req.params.id)
-	
 });
 
-// POST http-method /todos
-app.post('/todos', function(req, res){
-	var body=req.body;
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+	//_.pick to pick description and completed
 
-	// add id-field
-	body.id = todoNextId;
-	todoNextId++;
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// Set body.description to be trimmed value
+	body.description = body.description.trim();
+
+	// add id field
+	body.id = todoNextId++;
 
 	// push body into array
 	todos.push(body);
-
+	
 	res.json(body);
-
 });
 
-app.listen(PORT, function(){
-	console.log('Express listening on port ' + PORT + '!' );
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
 });
+
+

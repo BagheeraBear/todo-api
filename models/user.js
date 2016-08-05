50068ab1d53881739a713c159213bee0ea9b2e49
Ulf Swedin <ulf@swedin.nu>
2016-08-05 14:23:11 +0200

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,29 +1,52 @@
-module.exports = function(sequelize, DataTypes){
+var bcrypt = require('bcryptjs');
+//
+var _ = require('underscore');
+
+module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
-		email:{
+		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true,
-			validate:{
+			validate: {
 				isEmail: true
 			}
 		},
-		password:{
-			type: DataTypes.STRING,
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
-			validate:{
-				len:[7,100]
+			validate: {
+				len: [7, 100]
+			},
+			set: function (value) { 
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
-		hooks:{
-			beforeValidate: function(user,options){
-				//user.email
-				if(typeof user.email ==='string'){
-					user.email=user.email.toLowerCase();
+		hooks: {
+			beforeValidate: function (user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
-				
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
-}
+};
